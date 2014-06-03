07b6b84c553c97dff499f44f7529678b2c3106bf
diebels727 <diebelsalternative@hotmail.com>
2014-06-03 19:54:25 -0400

    Intercept request with NotFoundHandler

--- main.go
+++ main.go
@@ -16,8 +16,10 @@ var log_file *os.File
 var logger *log.Logger
 
 func Relay(response http.ResponseWriter,req *http.Request) {
-  //forward request to target server
   logger.Printf("[event-proxy] Issuing GET.")
+  //rewrite request
+  //send request to target server
+
   resp,_ := http.Get(TARGET)
   defer resp.Body.Close()
   logger.Printf("[event-proxy] Issued GET.")
@@ -26,12 +28,24 @@ func Relay(response http.ResponseWriter,req *http.Request) {
   //write target response to my response
 }
 
+// func Proxy(response *http.ResponseWriter,request *http.Request) {
+
+// }
+
+type Proxy struct {}
+
+func (p Proxy) ServeHTTP(response http.ResponseWriter,request *http.Request) {
+  logger.Printf("[event-proxy] Intercepting ...")
+  logger.Printf("[event-proxy] request: ",request)
+}
+
 func main() {
   log_file,_ = os.Create("proxy.log")
   logger = log.New(log_file,"",LOG_FORMAT)
   logger.Printf("[event-proxy] Starting up ...")
 
-  m := mux.NewRouter()
-  m.HandleFunc("/",Relay).Methods("GET")
-  http.ListenAndServe(":9091",m)
+  router := mux.NewRouter()
+  proxy := Proxy{}
+  router.NotFoundHandler = proxy
+  http.ListenAndServe(":9091",router)
 }
